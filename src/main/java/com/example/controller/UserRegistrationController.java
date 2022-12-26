f4e5472e5a51be22a09155f9977880316e2be198
mehedihasan03 <mdmehedi03@gmail.com>
2022-12-26 15:22:14 +0600

    token enabled for 15 minutes

--- src/main/java/com/example/controller/UserRegistrationController.java
+++ src/main/java/com/example/controller/UserRegistrationController.java
@@ -3,10 +3,7 @@ package com.example.controller;
 import com.example.request.RegistrationRequest;
 import com.example.service.RegistrationService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/registration")
@@ -19,5 +16,10 @@ public class UserRegistrationController {
     @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.registerUser(request);
+    }
+
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token){
+        return registrationService.confirmToken(token);
     }
 }
--- src/main/java/com/example/service/RegistrationService.java
+++ src/main/java/com/example/service/RegistrationService.java
@@ -3,9 +3,14 @@ package com.example.service;
 import com.example.appUser.AppUser;
 import com.example.appUser.AppUserRole;
 import com.example.request.RegistrationRequest;
+import com.example.token.ConfirmationToken;
+import com.example.token.ConfirmationTokenService;
 import com.example.utils.EmailValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
@@ -13,6 +18,7 @@ public class RegistrationService {
 
     private final AppUserService appUserService;
     private final EmailValidator emailValidator;
+    private final ConfirmationTokenService confirmationTokenService;
 
     public String registerUser(RegistrationRequest request) {
 
@@ -31,5 +37,31 @@ public class RegistrationService {
                         AppUserRole.USER
                 )
         );
+    }
+
+    @Transactional
+    public String confirmToken(String token){
+
+        ConfirmationToken confirmationToken = confirmationTokenService
+                .getToken(token)
+                .orElseThrow(
+                        () -> new IllegalStateException("token not found")
+                );
+
+        if (confirmationToken.getConfirmedAt() != null){
+            throw new IllegalStateException("email already confirmed");
+        }
+
+        LocalDateTime expiredAt = confirmationToken.getExpiresAt();
+
+        if(expiredAt.isBefore(LocalDateTime.now())){
+            throw new IllegalStateException("token expired");
+        }
+
+        confirmationTokenService.setConfirmedAt(token);
+        appUserService.enableAppUser(
+                confirmationToken.getAppUser().getEmail()
+        );
+        return "Confirmed";
     }
 }
